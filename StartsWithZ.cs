533d545ebfa780356ebd6f0dc472e84684c4ae8a
Taylor Somers <tsomers@yandex.com>
2020-07-12 17:57:04 -0400

    Add logic to determine and relay whether inputted user name starts with Z

--- StartsWithZ.cs
+++ StartsWithZ.cs
@@ -4,6 +4,15 @@ class Program
 {
   static void Main()
   {
-    
+    Console.WriteLine("What is your name?");
+    string userName = Console.ReadLine();
+    if (userName.StartsWith("Z"))
+    {
+      Console.WriteLine("Your name starts with a Z!");
+    }
+    else
+    {
+      Console.WriteLine("Your name doesn't start with a Z :(");
+    }
   }
 }
